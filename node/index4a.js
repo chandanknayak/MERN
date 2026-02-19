e8117c886f0572fcc49e25517417e38f77dcf4a7
chandankayak <chandannayak5517@gmail.com>
2026-02-19 13:59:09 +0530

    updated

--- node/index4a.js
+++ node/index4a.js
@@ -1,9 +1,31 @@
+// import express from 'express'
+// const app=express()
+// app.listen((8080),()=>console.log("Server started"));
+// app.get("/:num1/:num2",(req,res)=>{
+    // const n1 = Number(req.params.num1);
+    // const n2 = Number(req.params.num2);
+    // const sum=n1+n2
+    // res.send(sum);
+//      res.send("Good morning")
+// });
+
+// import express from 'express'
+// const app=express()
+// app.listen((8080),()=>console.log("Server started"));
+// app.get("/x/:num1/y/:num2/z/:num3",(req,res)=>{
+//     const n1 = Number(req.params.num1);
+//     const n2 = Number(req.params.num2);
+//     const n3 = Number(req.params.num3);
+//     const sum=n1+n2+n3
+//     res.send(sum);
+//     console.log(req.url)
+//});
+
+
 import express from 'express'
 const app=express()
 app.listen((8080),()=>console.log("Server started"));
-app.get("/:num1/:num2",(req,res)=>{
-    const n1 = Number(req.params.num1);
-    const n2 = Number(req.params.num2);
-    const sum=n1+n2
-    res.send(sum);
+app.get("/:a/:b/:c/:d",(req,res)=>{
+    
+     res.send("Hello Student")
 });
--- node/tempCodeRunnerFile.js
+++ node/tempCodeRunnerFile.js
@@ -1,3 +1,9 @@
-app.get("/:id/:email",(req,res)=>{
-//     res.send(req.params.id);
-// });
+import express from 'express'
+const app=express()
+app.listen((8080),()=>console.log("Server started"));
+app.get("/:num1/:num2",(req,res)=>{
+    const n1 = Number(req.params.num1);
+    const n2 = Number(req.params.num2);
+    const sum=n1+n2
+    res.send(sum);
+});
