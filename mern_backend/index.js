75acf741be31e6444af5ed46e7107c6e831c4b54
chandankayak <chandannayak5517@gmail.com>
2026-02-26 13:54:53 +0530

    updated

--- mern_backend/index.js
+++ mern_backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dbConnect from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 const app = express();
+
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
 const startServer = async () => {
   await dbConnect();
   app.listen(8080, () => console.log("Server started"));
 };
 startServer();
-app.use(express.json());
-app.use("/api/users", userRouter);
--- mern_backend/routes/productRoute.js
+++ mern_backend/routes/productRoute.js
@@ -1,9 +1,11 @@
 import {
+  addProduct,
   showProducts,
   deleteProduct,
 } from "../controllers/productController.js";
 import express from "express";
 const productRouter = express.Router();
+productRouter.post("/",addProduct)
 productRouter.get("/", showProducts);
 productRouter.delete("/:id", deleteProduct);
 export default productRouter;
