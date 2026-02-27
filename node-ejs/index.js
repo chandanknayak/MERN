1f58b28bc0d33c5df7af58e5a3b2fa0e7241b84b
chandankayak <chandannayak5517@gmail.com>
2026-02-27 13:43:11 +0530

    update

--- node-ejs/index.js
+++ node-ejs/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 import expressLayouts from "express-ejs-layouts";
 const app = express();
 app.use(expressLayouts)
@@ -9,7 +10,7 @@ app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const dbConnect = async () => {
-  await mongoose.connect("mongodb://localhost:27017/merndatabase");
+  await mongoose.connect("process.env.MONGO_URI");
 };
 const startServer = async () => {
   await dbConnect();
